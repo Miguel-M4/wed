635948a73c845a40fafadcfd575c30909f945ac7
gerarRB <gerarbarriostm@gmail.com>
2024-09-07 07:54:11 -0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+
+
+
+
+<template>
+  
+    <h1 class="text-2xl font-bold text-center text-gray-800" >Mi app</h1>
+    <hr />
+    <MiComponente :value="0"/>
+    <MiComponente2 :value="0"/>
+
+    
+
+
+
+</template>
+
+<script setup lang="ts">
+
+import MiComponente from './components/MiComponente.vue'
+import MiComponente2 from './components/MiComponente2.vue'
+
+
+</script>
+
+<style scoped></style>
---
+++ src/components/MiComponente.vue
@@ -0,0 +1,39 @@
+
+<template>
+    <h1 class="text-2xl font-bold text-center text-gray-800">Primer Componente</h1>
+
+    <div class="flex justify-center space-x-4">
+    <h3>Número - {{ numero }}</h3>
+    <button
+      class="btn"
+      @click="numero++"
+    >+1</button>
+    <button
+      class="btn"
+      @click="numero--"
+    >-1</button>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+interface Props {
+    value: number;
+}
+
+const props = defineProps<Props>();
+
+const numero = ref(props.value);
+
+// Creamos una variable reactiva para el número
+//const numero = ref(0)
+</script>
+
+<style scoped>
+h3 {
+    color: red;
+}
+</style>
+
+
