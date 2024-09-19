b5f39f3f24740d570ac2d99ec5cc51ef798e814f
Miguel-M4 <no01137273@uls.edu.sv>
2024-09-19 05:47:07 -0600

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,40 @@
 
 
-
-
 <template>
+    <div class="flex flex-col h-screen">
+      <!-- Header -->
+      <header class="flex items-center h-14 px-4 border-b border-gray-300 sm:h-16 md:px-6 lg:px-8">
+        <div>
+          <a class="flex items-center gap-2 font-semibold" href="#">
+            <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="40" height="40" />
+          </a>
+        </div>
+        <nav class="ml-auto space-x-4 flex items-center h-10 sm:space-x-6">
+            <RouterLink to="/">Principal</RouterLink>
+            <RouterLink :to="{ name: 'FeaturesPagina'  }">Feature</RouterLink>
+            <RouterLink to="/ContactoPagina">Contacto</RouterLink>
+            <RouterLink to="/PreciosPagina">Precios</RouterLink>
+        </nav>
+      </header>
+      <!-- Fin Header -->
   
-    <h1 class="text-2xl font-bold text-center text-gray-800" >Mi app</h1>
-    <hr />
-    <MiComponente :value="0"/>
-    <MiComponente2 :value="0"/>
+      <!-- Main -->
+      <main class="flex-1 flex items-center justify-center py-6">
+        <main>
+         <RouterView />
+        </main>
+      </main>
+      <!-- Fin Main -->
+  
+      <!-- Footer -->
+      <footer class="flex items-center h-14 px-4 border-t border-gray-300 sm:h-16 md:px-6 lg:px-8">
+        <p class="flex-1 text-sm text-gray-500 text-center">
+          © {{ new Date().getFullYear() }} 20xx Acme Corporation. Derechos reservados
+        </p>
+      </footer>
+      <!-- Fin Footer -->
+    </div>
+  </template>
+  
 
-    
 
-
-
-</template>
-
-<script setup lang="ts">
-
-import MiComponente from './components/MiComponente.vue'
-import MiComponente2 from './components/MiComponente2.vue'
-
-
-</script>
-
-<style scoped></style>
